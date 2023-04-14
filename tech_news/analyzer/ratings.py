17f0fd527c6cf7858dc737efa636083018511d68
Robert Santos <robertdosantos09@gmail.com>
2023-04-14 14:20:58 -0300

    feat: Adicionando codigos a função def top_5_categories. Para a resolução do requisito 10.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,17 @@
+from operator import itemgetter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    ranking = {}
+    for news in find_news():
+        if (not news['category'] in ranking):
+            ranking[news['category']] = 1
+        else:
+            ranking[news['category']] += 1
+    list_tuples = list(ranking.items())
+    list_tuples.sort(key=itemgetter(0))
+    list_result = sorted(list_tuples, key=itemgetter(1), reverse=True)
+    list_ranking = [value[0] for value in list_result]
+    return list_ranking[:5]
